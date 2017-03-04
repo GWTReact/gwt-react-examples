09d22e3b23dc1ae33e07208e8793c2fb796b094b
Paul Stockley <pstockley1@gmail.com>
2017-03-04 15:36:42 -0500

    Update to support ES6 Style components

--- src/gwt/react/preact_sanity_test/client/StatefulExample2.java
+++ src/gwt/react/preact_sanity_test/client/StatefulExample2.java
@@ -4,8 +4,8 @@ package gwt.react.preact_sanity_test.client;
 import gwt.interop.utils.client.plainobjects.JsPlainObj;
 import gwt.react.client.components.Component;
 import gwt.react.client.components.ComponentConstructorFn;
-import gwt.react.client.components.ComponentUtils;
 import gwt.react.client.elements.ReactElement;
+import gwt.react.client.events.MouseEvent;
 import gwt.react.client.proptypes.BaseProps;
 import gwt.react.client.proptypes.html.InputProps;
 import gwt.react.client.proptypes.html.LabelProps;
@@ -15,6 +15,7 @@ import jsinterop.annotations.JsType;
 
 import static gwt.interop.utils.client.plainobjects.JsPlainObj.$;
 import static gwt.react.client.api.React.DOM.*;
+import static gwt.react.client.components.ComponentUtils.getCtorFn;
 
 /**
  * This example shows a stateful component where we have renamed the JsType and added it to the global scope
@@ -31,6 +32,12 @@ public class StatefulExample2 extends Component<BaseProps, StatefulExample2.Stat
 		super(props);
 	}
 
+	void onClicked(MouseEvent e) {
+		//React recommends using the setState function if you are setting the new state based on the old state.
+		//possibly set state calls could be batched up and updated asynchronously
+		setState((prevState, props) -> $(new State(), "checked", !prevState.checked));
+	}
+
 	@Override
 	protected ReactElement<?, ?> render() {
 		return
@@ -43,7 +50,7 @@ public class StatefulExample2 extends Component<BaseProps, StatefulExample2.Stat
 						.id("checkField")
 						.type(InputType.checkbox)
 						.checked(state.checked)
-						.onClick(e -> setState($(new State(), "checked", !state.checked)))
+						.onClick(this::onClicked)
 			)
 		);
 	}
@@ -52,6 +59,6 @@ public class StatefulExample2 extends Component<BaseProps, StatefulExample2.Stat
 	//disabled class meta data using (-XdisableClassMetadata) . In this case you need to use ComponentUtils.getCtorFn
 	//to get the constructor function
 	public static ComponentConstructorFn<BaseProps> component() {
-		return ComponentUtils.getCtorFn("RenamedStatefulExample2");
+		return getCtorFn("RenamedStatefulExample2");
 	}
 }
